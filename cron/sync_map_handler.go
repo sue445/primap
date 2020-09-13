67014974f18fc5c3a5db283ea65e1b0886880989
sue445 <sue445@sue445.net>
2020-09-13 23:21:17 +0900

    Impl syncMap

--- cron/sync_map_handler.go
+++ cron/sync_map_handler.go
@@ -2,10 +2,56 @@ package cron
 
 import (
 	"fmt"
+	"github.com/itchyny/timefmt-go"
+	"github.com/sue445/primap/entity"
+	"github.com/sue445/primap/prismdb"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 // SyncMapHandler returns handler of /cron/sync_map
 func SyncMapHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "ok")
+}
+
+func syncMap(time time.Time) error {
+	client, err := prismdb.NewClient()
+	if err != nil {
+		return err
+	}
+
+	shops, err := client.GetAllShops()
+	if err != nil {
+		return err
+	}
+
+	var entityShops []*entity.ShopEntity
+	for _, shop := range shops {
+		entityShops = append(entityShops, toEntity(shop))
+	}
+
+	revision := timefmt.Format(time, "%Y%m%d-%H%M%S")
+
+	projectID := os.Getenv("GCP_PROJECT")
+	dao := entity.NewShopDao(projectID)
+
+	err = dao.SaveShops(entityShops, revision)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Successful: revision=%s, shops=%d\n", revision, len(entityShops))
+
+	return nil
+}
+
+func toEntity(shop *prismdb.Shop) *entity.ShopEntity {
+	return &entity.ShopEntity{
+		Name:       shop.Name,
+		Prefecture: shop.Prefecture,
+		Address:    shop.Address,
+		Series:     shop.Series,
+	}
 }

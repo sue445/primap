cfba2c8f4677550a665959d0b5d585aebcd06070
sue445 <sue445@sue445.net>
2020-09-20 01:29:00 +0900

    Impl SubtractSlice, Contains

---
+++ util/slice.go
@@ -0,0 +1,27 @@
+package util
+
+// SubtractSlice returns subtracted slice (src - sub)
+func SubtractSlice(src []string, sub []string) []string {
+	var ret []string
+
+	for _, s := range src {
+		if !Contains(sub, s) {
+			ret = append(ret, s)
+		}
+	}
+
+	return ret
+}
+
+// Contains returns whether slice contains item
+func Contains(slice []string, item string) bool {
+	// c.f. https://stackoverflow.com/questions/10485743/contains-method-for-a-slice
+
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}

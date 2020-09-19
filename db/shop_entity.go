5be79a476ba9e6cd6c0ce903f53041d9d39762fd
sue445 <sue445@sue445.net>
2020-09-19 11:45:20 +0900

    Impl UpdateAddressWithLocation

--- db/shop_entity.go
+++ db/shop_entity.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"context"
+	"github.com/sue445/primap/config"
 	"google.golang.org/genproto/googleapis/type/latlng"
+	"googlemaps.github.io/maps"
 	"time"
 )
 
@@ -18,4 +21,35 @@ type ShopEntity struct {
 	CreatedAt  time.Time      `firestore:"created_at"`
 	UpdatedAt  time.Time      `firestore:"updated_at"`
 	Location   *latlng.LatLng `firestore:"latlng"`
+}
+
+// UpdateAddressWithLocation update address and fetch location if necessary
+func (e *ShopEntity) UpdateAddressWithLocation(address string) error {
+	if e.Address == address || config.GetGoogleMapsAPIKey() == "" {
+		return nil
+	}
+
+	c, err := maps.NewClient(maps.WithAPIKey(config.GetGoogleMapsAPIKey()))
+
+	if err != nil {
+		return err
+	}
+
+	r := &maps.GeocodingRequest{
+		Address: address,
+	}
+
+	resp, err := c.Geocode(context.Background(), r)
+
+	if err != nil {
+		return err
+	}
+
+	e.Address = address
+	e.Location = &latlng.LatLng{
+		Latitude:  resp[0].Geometry.Location.Lat,
+		Longitude: resp[0].Geometry.Location.Lng,
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	cloud.google.com/go/firestore v1.3.0
 	github.com/getsentry/sentry-go v0.7.0
 	github.com/gorilla/mux v1.8.0
-	github.com/itchyny/timefmt-go v0.1.1
 	github.com/jarcoal/httpmock v1.0.6
 	github.com/knakk/digest v0.0.0-20160404164910-fd45becddc49 // indirect
 	github.com/knakk/rdf v0.0.0-20190304171630-8521bf4c5042 // indirect
@@ -14,4 +13,5 @@ require (
 	github.com/stretchr/testify v1.6.1
 	google.golang.org/api v0.29.0
 	google.golang.org/genproto v0.0.0-20200728010541-3dc8dca74b7b
+	googlemaps.github.io/maps v1.2.2
 )

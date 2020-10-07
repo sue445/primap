48c781c293db70fa9294174623a61d8775fe57e3
sue445 <sue445@sue445.net>
2020-10-07 23:40:35 +0900

    Add tailwind to webpack.config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 const webpack = require('webpack');
 
@@ -17,6 +19,12 @@ module.exports = {
       'REACT_APP_GOOGLE_BROWSER_API_KEY',
       'REACT_APP_SENTRY_DSN',
     ]),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
   ],
   output: {
     path: __dirname + '/public',
@@ -27,11 +35,35 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '/public/',
+            },
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins: [
+                  require('tailwindcss'),
+                  require('autoprefixer'),
+                ],
+              },
+            }
+          },
+        ],
+      },
+    ],
   },
 }
 
-if (process.env.NODE_ENV != "production") {
+if (devMode) {
   module.exports.devtool = "inline-source-map"
 }

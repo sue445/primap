3ecfd292abfb4f49bb4499f8f3e7bcb84029a2e0
sue445 <sue445@sue445.net>
2020-10-23 23:16:50 +0900

    Add test of getAndPublishShops

--- function/handler_cron_update_shops_test.go
+++ function/handler_cron_update_shops_test.go
@@ -3,15 +3,41 @@ package primap
 import (
 	"cloud.google.com/go/pubsub"
 	"context"
+	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 	"github.com/sue445/primap/prismdb"
 	"github.com/sue445/primap/testutil"
+	"os"
 	"testing"
 
 	"cloud.google.com/go/pubsub/pstest"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 )
+
+func Test_getAndPublishShops(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("POST", "https://prismdb.takanakahiko.me/sparql",
+		httpmock.NewStringResponder(200, testutil.ReadTestData("testdata/all_shops.json")))
+
+	ctx := context.Background()
+	projectID := os.Getenv("PUBSUB_PROJECT_ID")
+
+	client, err := pubsub.NewClient(ctx, projectID)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	_, err = client.CreateTopic(ctx, topicID)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	err = getAndPublishShops(ctx, projectID)
+	assert.NoError(t, err)
+}
 
 func Test_publishShop(t *testing.T) {
 	srv := pstest.NewServer()

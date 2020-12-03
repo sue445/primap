9ca894caae5a741e424a5c94660ad6bc987866b8
sue445 <sue445@sue445.net>
2020-12-04 00:36:29 +0900

    Refactor correctLongitude

--- frontend/app/components/Util.ts
+++ frontend/app/components/Util.ts
@@ -1,4 +1,8 @@
 export function correctLongitude(longitude: number): number {
+  if (-180 < longitude && longitude < 180) {
+    return longitude;
+  }
+
   longitude += 180;
   while (longitude < 0) {
     longitude += 360;

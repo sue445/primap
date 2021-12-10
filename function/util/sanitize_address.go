0269ca76286ae727fa8fe3cd8c9e6bdef635abe2
sue445 <sue445@sue445.net>
2021-12-10 22:17:43 +0900

    Remove AEON in address

--- function/util/sanitize_address.go
+++ function/util/sanitize_address.go
@@ -18,9 +18,12 @@ func SanitizeAddress(address string) string {
 	sanitized = regexp.MustCompile(`([0-9]+)(?:番地)?の([0-9]+)`).ReplaceAllString(sanitized, "$1-$2")
 	sanitized = regexp.MustCompile(`([0-9]+)(?:(?:丁目)|(?:番地?)|(?:号))`).ReplaceAllString(sanitized, "$1-")
 	sanitized = regexp.MustCompile(`-([^0-9]|$)`).ReplaceAllString(sanitized, "$1")
+	sanitized = regexp.MustCompile(`イオン.+$`).ReplaceAllString(sanitized, "")
 
 	// Remove building name after street name
 	sanitized = regexp.MustCompile(`([0-9]+(?:-[0-9]+)?(?:-[0-9]+)?)[^-0-9条線].*$`).ReplaceAllString(sanitized, "$1")
+
+	sanitized = strings.TrimSpace(sanitized)
 
 	return sanitized
 }

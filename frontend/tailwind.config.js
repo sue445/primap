b6d1fdefc0328edceb7fa08d08d641e9b2d8f8d6
sue445 <sue445@sue445.net>
2022-10-04 23:41:09 +0900

    Fixed deprecation warning
    ```
    $ npm run build
    
    > primap-frontend@0.0.0 build
    > webpack
    
    warn - The `purge`/`content` options have changed in Tailwind CSS v3.0.
    warn - Update your configuration file to eliminate this warning.
    warn - https://tailwindcss.com/docs/upgrade-guide#configure-content-sources
    ```

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [
+  content: [
     './app/**/*.tsx',
     './app/**/*.ts',
     './app/**/*.css',

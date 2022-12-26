efc1c87b1941751bb120010a9f4de6cea1d166d0
sue445 <sue445@sue445.net>
2022-12-26 22:54:56 +0900

    Fix test

--- function/prishops/main_test.go
+++ function/prishops/main_test.go
@@ -25,10 +25,10 @@ func TestGetAllShops(t *testing.T) {
 	}
 
 	wantShop := &Shop{
-		Name:       "NICOPAイオン伊勢",
+		Name:       "モーリーファンタジー伊勢ララパーク",
 		Prefecture: "三重県",
-		Address:    "三重県伊勢市楠部町乙160 ｲｵﾝ伊勢店",
-		Series:     []string{"primagi"},
+		Address:    "三重県伊勢市小木町曽弥538 ｲｵﾝﾗﾗﾊﾟｰｸSC2階",
+		Series:     []string{"primagi_1"},
 	}
 	assert.Equal(t, wantShop, gotShops[0])
 
@@ -36,7 +36,7 @@ func TestGetAllShops(t *testing.T) {
 		Name:       "プリズムストーンカフェ原宿店",
 		Prefecture: "東京都",
 		Address:    "東京都渋谷区神宮前3-18-27 2F",
-		Series:     []string{"primagi", "prismstone", "pripara"},
+		Series:     []string{"primagi_1", "prismstone", "pripara"},
 	}
-	assert.Equal(t, wantShop2, gotShops[983])
+	assert.Equal(t, wantShop2, gotShops[1016])
 }
